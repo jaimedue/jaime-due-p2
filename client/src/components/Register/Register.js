8e0031df9e8c16100b92a26615f0637769ccb0f0
Mike Hunsicker <hunsickm@matc.edu>
2019-11-03 17:44:39 -0600

    Completed Activity 8

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-const Register = () => {
+const Register = ({ authenticateUser }) => {
+  let history = useHistory();
   const [userData, setUserData] = useState({
     name: '',
     email: '',
     password: '',
     passwordConfirm: ''
   });
+  const [errorData, setErrorData] = useState({ errors: null });
 
   const { name, email, password, passwordConfirm } = userData;
+  const { errors } = errorData;
 
   const onChange = e => {
     const { name, value } = e.target;
@@ -19,7 +23,7 @@ const Register = () => {
     })
   }
 
-  const register = async () => {
+  const registerUser = async () => {
     if (password !== passwordConfirm) {
       console.log('Passwords do not match');
     }
@@ -39,11 +43,21 @@ const Register = () => {
 
         const body = JSON.stringify(newUser);
         const res = await axios.post('http://localhost:5000/api/users', body, config);
-        console.log(res.data);
+        
+        // Store user data and redirect
+        localStorage.setItem('token', res.data.token);
+        history.push('/');
       } catch (error) {
-        console.error(error.response.data);
-        return;
+        // Clear user data and set errors
+        localStorage.removeItem('token');
+        
+        setErrorData({
+          ...errors,
+          errors: error.response.data.errors
+        })
       }
+
+      authenticateUser();
     }
   }
 
@@ -83,7 +97,11 @@ const Register = () => {
           onChange={e => onChange(e)} />
       </div>
       <div>
-        <button onClick={() => register()}>Register</button> 
+        <button onClick={() => registerUser()}>Register</button> 
+      </div>
+      <div>
+        {errors && errors.map(error => 
+          <div key={error.msg}>{error.msg}</div>)}
       </div>
     </div>
   )
